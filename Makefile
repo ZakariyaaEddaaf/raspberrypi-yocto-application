2ca00e0595424eea18cdcc5022f56f940c1cb34a
zakariyaa.eddaaf <zakariyaa.eddaaf@evologics.de>
2024-06-17 22:55:17 +0200

    TCP GNSS Server application

--- Makefile
+++ Makefile
@@ -8,23 +8,28 @@ RELEASEFLAGS = -O3 -DNDEBUG
 # Directories
 UTILSDIR = utils
 GNSSDIR = global-positioning-system
+TCPDIR = transmission-control-protocol
 
 # Source files
 SRCS_UTILS = $(UTILSDIR)/serial.cpp
 SRCS_GPS = $(GNSSDIR)/gnss.cpp
+SRCS_TCP = $(TCPDIR)/server.cpp
 SRCS_MAIN = main.cpp
 
 # Header files
 HEADERS_UTILS = $(wildcard $(UTILSDIR)/*.hpp)
 HEADERS_GPS = $(wildcard $(GNSSDIR)/*.hpp)
+HEADERS_TCP = $(wildcard $(TCPDIR)/*.hpp)
+
 
 # Object files
 OBJS_UTILS = $(SRCS_UTILS:.cpp=.o)
 OBJS_GPS = $(SRCS_GPS:.cpp=.o)
+OBJS_TCP = $(SRCS_TCP:.cpp=.o)
 OBJ_MAIN = $(SRCS_MAIN:.cpp=.o)
 
 # Executable
-TARGET = gps_server
+TARGET = gnss_server
 
 # Default target
 all: release
@@ -38,15 +43,15 @@ release: CXXFLAGS += $(RELEASEFLAGS)
 release: $(TARGET)
 
 # Linking
-$(TARGET): $(OBJS_UTILS) $(OBJS_GPS) $(OBJ_MAIN)
-	$(CXX) $(CXXFLAGS) -o $(TARGET) $(OBJS_UTILS) $(OBJS_GPS) $(OBJ_MAIN)
+$(TARGET): $(OBJS_UTILS) $(OBJS_GPS) $(OBJS_TCP) $(OBJ_MAIN)
+	$(CXX) $(CXXFLAGS) -o $(TARGET) $(OBJS_UTILS) $(OBJS_GPS) $(OBJS_TCP) $(OBJ_MAIN)
 
 # Compile source files into object files
-%.o: %.cpp $(HEADERS_UTILS) $(HEADERS_GPS)
+%.o: %.cpp $(HEADERS_UTILS) $(HEADERS_GPS) $(HEADERS_TCP)
 	$(CXX) $(CXXFLAGS) -c $< -o $@
 
 # Clean target
 clean:
-	rm -f $(OBJS_UTILS) $(OBJS_GPS) $(OBJ_MAIN) $(TARGET)
+	rm -f $(OBJS_UTILS) $(OBJS_GPS) $(OBJ_MAIN) $(OBJS_TCP) $(TARGET)
 
 .PHONY: all debug release clean
--- main.cpp
+++ main.cpp
@@ -1,55 +1,19 @@
-#include "utils/serial.hpp"
-#include "global-positioning-system/gnss.hpp"
-#include <iostream>
-#include <thread>
-#include <sstream>
+#include "transmission-control-protocol/server.hpp"
 
 int main()
 {
-    // Create an instance of the Serial class
-    Serial serial("/dev/ttyACM0", 115200);
-    // Create an instance of the GNSS class
-    GNSS gnss;
+    const std::string server_address = "192.168.0.2";
+    int server_port = 5000;
+    const std::string serial_port = "/dev/ttyACM0";
+    uint32_t baud_rate = 115200;
 
-    // Open the serial port
-    if (!serial.open_serial())
+    Server server(server_address, server_port, serial_port, baud_rate);
+
+    if (!server.start_server())
     {
-        std::cerr << "Failed to open serial port." << std::endl;
-        return 1;
+        std::cerr << "Failed to start server." << std::endl;
+        return -1;
     }
-
-    // Buffer to store GPS data
-    char buffer[1024];
-
-    while (true)
-    {
-        // Read data from the serial port
-        ssize_t n = serial.read_serial(buffer, sizeof(buffer) - 1);
-        if (n < 0)
-        {
-            std::cerr << "Failed to read from serial port." << std::endl;
-            break;
-        }
-
-        buffer[n] = '\0'; // Null-terminate the string
-        std::string data(buffer);
-
-        // Find GNGGA sentence from the data
-        std::string gga_sentence = gnss.catch_gpgga_gngga(data);
-
-        // Check if a valid GGA sentence is found
-        if (!gga_sentence.empty())
-        {
-            //std::cout << gga_sentence << std::endl;
-            gnss.parse_gpgga_gngga(gga_sentence);
-            std::cout<<gnss.get_hdop()<<std::endl;
-
-        }
-
-    }
-
-    // Close the serial port
-    serial.close_serial();
 
     return 0;
 }
